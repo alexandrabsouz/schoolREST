9ad92b544bb4a5f5a15f45a8f853d287d2a3ebd4
alexandra <alexandrabsouz@gmail.com>
2021-09-01 10:52:40 -0300

    feat: criando views de cursos e avaliacoes

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.serializers import Serializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
+
+
+class CursoAPIView(APIView):
+    '''
+    API para mostrar os cursos
+    '''
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+    
+
+class AvaliacaoAPIView(APIView):
+    '''
+    API para mostrar as avaliaçoes dos cursos
+    '''
+    
+    def get(self, request):
+        avaliacoes = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serializer.data)
